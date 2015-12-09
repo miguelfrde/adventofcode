674af5f8360c924319711b03ccdf547a25395bd4
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-12-09 11:13:47 -0600

    Problem 4 finished

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: part1 part2
 
-part1:
+part1: part1.hs
 	ghc -o part1 part1.hs
 
-part2:
+part2: part2.hs
 	ghc -o part2 part2.hs
 
 clean:
---
+++ problem4/part2.hs
@@ -0,0 +1,20 @@
+import System.Environment
+import Data.Digest.Pure.MD5
+import Data.ByteString.Lazy.Char8
+
+isProperHash :: String -> Bool
+isProperHash string = (pack "000000") `isPrefixOf` (pack string)
+
+solve' :: String -> Integer -> Integer
+solve' baseKey num
+    | isProperHash md5Hash = num
+    | otherwise            = solve' baseKey (num + 1)
+    where key = baseKey ++ (show num)
+          md5Hash = show . md5 . pack $ key
+
+solve :: String -> Integer
+solve key = solve' key 0
+
+main = do
+    [word] <- getArgs
+    Prelude.putStrLn . show . solve $ word
